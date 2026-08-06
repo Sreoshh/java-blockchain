c08f18e45e3cfdf0e8f6b78dbd4b3990497658a3
Sreoshi Chakraborty <Iamsreoshi19@gmail.com>
2026-08-06 11:02:05 +0530

    Right now the walley only had keys but now it will be able to check its balance, collect its own UTXOs and generate transactions.

--- src/Wallet.java
+++ src/Wallet.java
@@ -1,4 +1,7 @@
 import java.security.*;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
 
 public class Wallet {
 
@@ -7,6 +10,9 @@ public class Wallet {
 
     // Private key (keep this secret!)
     public PrivateKey privateKey;
+
+    // Unspent transaction outputs owned by this wallet
+    public HashMap<String, TransactionOutput> UTXOs = new HashMap<>();
 
     // Constructor
     public Wallet() {
@@ -36,5 +42,67 @@ public class Wallet {
         }
 
     }
+
+    public float getBalance(Blockchain blockchain) {
+
+    float total = 0;
+
+    UTXOs.clear();
+
+    for (Map.Entry<String, TransactionOutput> item : blockchain.UTXOs.entrySet()) {
+
+        TransactionOutput UTXO = item.getValue();
+
+        if (UTXO.isMine(publicKey)) {
+
+            UTXOs.put(UTXO.id, UTXO);
+
+            total += UTXO.value;
+        }
+    }
+
+    return total;
+}
+
+
+    public Transaction sendFunds(Blockchain blockchain, PublicKey recipient, float value) {
+
+    if (getBalance(blockchain) < value) {
+
+        System.out.println("Not enough funds.");
+        return null;
+
+    }
+
+    ArrayList<TransactionInput> inputs = new ArrayList<>();
+
+    float total = 0;
+
+    for (Map.Entry<String, TransactionOutput> item : UTXOs.entrySet()) {
+
+        TransactionOutput UTXO = item.getValue();
+
+        total += UTXO.value;
+
+        inputs.add(new TransactionInput(UTXO.id));
+
+        if (total >= value)
+            break;
+
+    }
+
+    Transaction newTransaction = new Transaction(publicKey, recipient, value, inputs);
+
+    newTransaction.generateSignature(privateKey);
+
+    for (TransactionInput input : inputs) {
+
+        UTXOs.remove(input.transactionOutputId);
+
+    }
+
+    return newTransaction;
+
+}
 
 }
