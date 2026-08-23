d2380d60b9942cc2363cfea4113a5ba2a63d96d0
Sreoshi Chakraborty <Iamsreoshi19@gmail.com>
2026-08-23 23:48:37 +0530

    feat: integrate Spring Boot

--- src/main/java/com/BlockchainController.java
+++ src/main/java/com/BlockchainController.java
@@ -6,8 +6,24 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class BlockchainController {
 
+    private final Blockchain blockchain;
+
+    public BlockchainController() {
+        blockchain = new Blockchain();
+    }
+
     @GetMapping("/")
     public String home() {
         return "Java Blockchain API is running!";
+    }
+
+    @GetMapping("/api/blockchain")
+    public Blockchain getBlockchain() {
+        return blockchain;
+    }
+
+    @GetMapping("/api/blockchain/validate")
+    public boolean validateBlockchain() {
+        return blockchain.isChainValid();
     }
 }
